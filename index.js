73e5d87c8dc8715db68f551665a5991dd2eea538
Abiyasa <abiyasa2410@gmail.com>
2020-08-15 15:23:15 +0700

    disable static and views

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+// app.use(express.static(path.join(__dirname, 'public')))
+// app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', 'ejs')
 // app.get('/', (req, res) => res.render('pages/index'))
 app.get('/', (req, res) => res.send({ ok: 'root' }))
 app.get('/ping', (req, res) => res.send({ ok: 'pong' }))
